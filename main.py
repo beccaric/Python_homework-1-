37e19d6e0e2af05f779a142943531b41e647ff47
becca <93043262+beccaric@users.noreply.github.com>
2022-10-02 15:10:39 +0000

    Homework task 1

---
+++ main.py
@@ -0,0 +1,14 @@
+#Python Homework: Task 1 (Python Fundamentals: Part 1)
+
+#HLT1: Favourite Food
+
+name = input("What is your name?")
+print("Hello" + name)
+
+starter = input("What is your favourite starter?")
+maincourse = input("What is your favourite main course?")
+dessert = input("What is your favourite dessert?")
+drink = input("What is your favourite drink?")
+
+print("So your favourite meal is",maincourse,"with a glass of",drink,)
+
